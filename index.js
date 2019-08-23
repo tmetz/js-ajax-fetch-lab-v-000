5ee0ea54a0022378fbad3e965ec1c77ac368a9c0
Tammy Metz <tammy@tammymetz.com>
2019-08-23 08:51:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,4 +31,11 @@ function createIssue() {
 
 function getIssues() {
   //once an issue is submitted, fetch all open issues to see the issues you are creating
+  const repo = `${user}/js-ajax-fetch-lab`;
+  const url = `${baseURL}/repos/${repo}/issues`;
+  fetch(url, {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json()).then(json => console.log(json));
 }
